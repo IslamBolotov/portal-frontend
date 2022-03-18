b06c75137eb48a3c7673602c2ce2704374ea4df6
islam <islambolotov99@gmail.com>
2022-03-18 16:49:06 +0600

    first commit

---
+++ src/containers/NoPage/index.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import {withRouter} from 'react-router-dom';
+
+const ChatInput = () => {
+    return (
+      <h1>No Page</h1>
+  );
+};
+
+export default withRouter(ChatInput);
